305cd2338a3ddfc27a4f2dc4c71d96c57e921f31
suvarchal <suvarchal.kumar@gmail.com>
2020-11-06 03:38:25 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+# read the contents of your README file
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="pysftp",
+    version="0.0.2",
+    author="Suvarchal K. Cheedela",
+    author_email="suvarchal.kumar@gmail.com",
+    description="Asynchronous sftp",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/suvarchal/pysftp",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    install_requires=[
+        'Click', 'tqdm', 'asyncssh'
+    ],
+    entry_points='''
+        [console_scripts]
+        pysftp=src.cli:cli
+    ''',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT",
+        "Operating System :: Linux",
+    ],
+)
